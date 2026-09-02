ea480aa4ec6d08c79e9a5c29b7c2d51cc8e407df
rindunadhirah <rindunadhirah@gmail.com>
2026-09-02 14:18:38 +0700

    correction allowed host

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1, rindu-maharani-myportofolio.pws.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["ALLOWED_HOSTS", "127.0.0.1, rindu-maharani-myportofolio.pws.cs.ui.ac.id"]
 
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
